aab2fcd371b75b68af156fda82abb4880de26058
Seth Guberman <sguberman@gmail.com>
2016-11-19 14:00:55 -0500

    add a label to the gui form

--- gui.py
+++ gui.py
@@ -1,10 +1,13 @@
 import tkinter as tk
-
+from tkinter import ttk
 
 win = tk.Tk()
 win.title('Python GUI')
 
 win.resizable(0, 0)
+
+# Adding a label
+ttk.Label(win, text="A Label").grid(column=0, row=0)
 
 win.mainloop()
 
