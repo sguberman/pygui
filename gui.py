b9175a72fefa9270c125525b971eb10109ec1269
Seth Guberman <sguberman@gmail.com>
2016-11-19 15:03:25 -0500

    refactor and DRY up radiobutton widgets

--- gui.py
+++ gui.py
@@ -52,27 +52,20 @@ check3.grid(column=2, row=4, sticky=tk.W)
 COLOR1 = 'Blue'
 COLOR2 = 'Gold'
 COLOR3 = 'Red'
+colors = [COLOR1, COLOR2, COLOR3]
 
 # Radiobutton callback
 def radCall():
     radSel = radVar.get()
-    if radSel == 1:
-        win.configure(background=COLOR1)
-    elif radSel == 2:
-        win.configure(background=COLOR2)
-    elif radSel == 3:
-        win.configure(background=COLOR3)
+    win.configure(background=radSel)
 
-# Add three radio buttons
-radVar = tk.IntVar()
-rad1 = tk.Radiobutton(win, text=COLOR1, variable=radVar, value=1, command=radCall)
-rad1.grid(column=0, row=5, sticky=tk.W)
-
-rad2 = tk.Radiobutton(win, text=COLOR2, variable=radVar, value=2, command=radCall)
-rad2.grid(column=1, row=5, sticky=tk.W)
-
-rad3 = tk.Radiobutton(win, text=COLOR3, variable=radVar, value=3, command=radCall)
-rad3.grid(column=2, row=5, sticky=tk.W)
+# Add three radio buttons in a loop
+radVar = tk.StringVar()
+radVar.set('None')
+for i, color in enumerate(colors):
+    rad = tk.Radiobutton(
+        win, text=color, variable=radVar, value=color, command=radCall)
+    rad.grid(column=i, row=5, sticky=tk.W)
 
 # Using a scrolled text control
 scrolW = 30
