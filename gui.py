d15fd4f3c8fea3820eb8ac5c37e0bb9fb4adb651
Seth Guberman <sguberman@gmail.com>
2016-11-19 14:45:25 -0500

    add three radiobutton widgets that change the color of the window background

--- gui.py
+++ gui.py
@@ -46,5 +46,31 @@ check3 = tk.Checkbutton(win, text='Enabled', variable=chVarEn)
 check3.select()
 check3.grid(column=2, row=4, sticky=tk.W)
 
+# Radiobutton globals
+COLOR1 = 'Blue'
+COLOR2 = 'Gold'
+COLOR3 = 'Red'
+
+# Radiobutton callback
+def radCall():
+    radSel = radVar.get()
+    if radSel == 1:
+        win.configure(background=COLOR1)
+    elif radSel == 2:
+        win.configure(background=COLOR2)
+    elif radSel == 3:
+        win.configure(background=COLOR3)
+
+# Add three radio buttons
+radVar = tk.IntVar()
+rad1 = tk.Radiobutton(win, text=COLOR1, variable=radVar, value=1, command=radCall)
+rad1.grid(column=0, row=5, sticky=tk.W)
+
+rad2 = tk.Radiobutton(win, text=COLOR2, variable=radVar, value=2, command=radCall)
+rad2.grid(column=1, row=5, sticky=tk.W)
+
+rad3 = tk.Radiobutton(win, text=COLOR3, variable=radVar, value=3, command=radCall)
+rad3.grid(column=2, row=5, sticky=tk.W)
+
 win.mainloop()
 
