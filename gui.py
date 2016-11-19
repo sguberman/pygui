54e360cc2728f7043d6aaa1d6fe0dc294c484597
Seth Guberman <sguberman@gmail.com>
2016-11-19 15:34:54 -0500

    Place all widgets in a labelframe to control the layout

--- gui.py
+++ gui.py
@@ -8,43 +8,51 @@ win.title('Python GUI')
 
 #win.resizable(0, 0)
 
+# Create a container frame
+monty = ttk.LabelFrame(win, text=' Monty Python ')
+monty.grid(column=0, row=0)
+
 # Modify button click function
 def clickMe():
     action.configure(text='Hello ' + name.get() + ' ' + numberChosen.get())
 
-action = ttk.Button(win, text='Click Me!', command=clickMe)
+action = ttk.Button(monty, text='Click Me!', command=clickMe)
 action.grid(column=2, row=1)
 
 # Change the label
-ttk.Label(win, text='Enter a name:').grid(column=0, row=0)
+ttk.Label(monty, text='Enter a name:').grid(column=0, row=0, sticky='W')
 
 # Add a textbox entry widget
 name = tk.StringVar()
-nameEntered = ttk.Entry(win, width=12, textvariable=name)
-nameEntered.grid(column=0, row=1)
+nameEntered = ttk.Entry(monty, width=12, textvariable=name)
+nameEntered.grid(column=0, row=1, sticky='W')
 nameEntered.focus()
 
 # Add a combobox widget
-ttk.Label(win, text='Choose a number:').grid(column=1, row=0)
+ttk.Label(monty, text='Choose a number:').grid(column=1, row=0)
 number = tk.StringVar()
-numberChosen = ttk.Combobox(win, width=12, textvariable=number, state='readonly')
+numberChosen = ttk.Combobox(
+    monty, width=12, textvariable=number, state='readonly')
 numberChosen['values'] = (1, 2, 4, 42, 100)
 numberChosen.grid(column=1, row=1)
 numberChosen.current(0)
 
 # Add three checkboxes
 chVarDis = tk.IntVar()
-check1 = tk.Checkbutton(win, text='Disabled', variable=chVarDis, state='disabled')
+check1 = tk.Checkbutton(
+    monty, text='Disabled', variable=chVarDis, state='disabled')
 check1.select()
 check1.grid(column=0, row=4, sticky=tk.W)
 
 chVarUn = tk.IntVar()
-check2 = tk.Checkbutton(win, text='Unchecked', variable=chVarUn)
+check2 = tk.Checkbutton(
+    monty, text='Unchecked', variable=chVarUn)
 check2.deselect()
 check2.grid(column=1, row=4, sticky=tk.W)
 
 chVarEn = tk.IntVar()
-check3 = tk.Checkbutton(win, text='Enabled', variable=chVarEn)
+check3 = tk.Checkbutton(
+    monty, text='Enabled', variable=chVarEn)
 check3.select()
 check3.grid(column=2, row=4, sticky=tk.W)
 
@@ -64,18 +72,18 @@ radVar = tk.StringVar()
 radVar.set('None')
 for i, color in enumerate(colors):
     rad = tk.Radiobutton(
-        win, text=color, variable=radVar, value=color, command=radCall)
-    rad.grid(column=i, row=5, sticky=tk.W)
+        monty, text=color, variable=radVar, value=color, command=radCall)
+    rad.grid(column=i, row=6, sticky=tk.W)
 
 # Using a scrolled text control
 scrolW = 30
 scrolH = 3
-scr = scrolledtext.ScrolledText(win, width=scrolW, height=scrolH, wrap=tk.WORD)
-scr.grid(column=0, columnspan=3, sticky='WE')
+scr = scrolledtext.ScrolledText(monty, width=scrolW, height=scrolH, wrap=tk.WORD)
+scr.grid(column=0, row=5, columnspan=3, sticky='WE')
 
 # Create a container to hold labels
-labelsFrame = ttk.LabelFrame(win, text=' Labels in a Frame ')
-labelsFrame.grid(column=0, row=7, padx=20, pady=40)
+labelsFrame = ttk.LabelFrame(monty, text=' Labels in a Frame ')
+labelsFrame.grid(column=0, row=7, padx=5, pady=5)
 
 # Place labels into the container element
 ttk.Label(labelsFrame, text='Label1').grid(column=0, row=0)
