cc813675c0293cb7d6e1a11f7e5890bc3d78eef2
Seth Guberman <sguberman@gmail.com>
2016-11-19 14:06:00 -0500

    add a button that changes text when clicked

--- gui.py
+++ gui.py
@@ -6,8 +6,17 @@ win.title('Python GUI')
 
 win.resizable(0, 0)
 
-# Adding a label
-ttk.Label(win, text="A Label").grid(column=0, row=0)
+# Modify adding a label
+aLabel = ttk.Label(win, text="A Label")
+aLabel.grid(column=0, row=0)
+
+# Button click Event Callback Function
+def clickMe():
+    action.configure(text='** I have been clicked! **')
+    aLabel.configure(foreground='red')
+
+action = ttk.Button(win, text='Click Me!', command=clickMe)
+action.grid(column=1, row=0)
 
 win.mainloop()
 
