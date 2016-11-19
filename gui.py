01daba7a9d675217fdf3c8ff752cb5bac627a65e
Seth Guberman <sguberman@gmail.com>
2016-11-19 14:12:53 -0500

    add a textbox widget

--- gui.py
+++ gui.py
@@ -6,17 +6,20 @@ win.title('Python GUI')
 
 win.resizable(0, 0)
 
-# Modify adding a label
-aLabel = ttk.Label(win, text="A Label")
-aLabel.grid(column=0, row=0)
-
-# Button click Event Callback Function
+# Modify button click function
 def clickMe():
-    action.configure(text='** I have been clicked! **')
-    aLabel.configure(foreground='red')
+    action.configure(text='Hello ' + name.get())
 
 action = ttk.Button(win, text='Click Me!', command=clickMe)
-action.grid(column=1, row=0)
+action.grid(column=1, row=1)
+
+# Change the label
+ttk.Label(win, text='Enter a name:').grid(column=0, row=0)
+
+# Add a textbox entry widget
+name = tk.StringVar()
+nameEntered = ttk.Entry(win, width=12, textvariable=name)
+nameEntered.grid(column=0, row=1)
 
 win.mainloop()
 
