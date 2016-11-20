0d0713ce66f3719282c2bfa666786777e211320c
Seth Guberman <sguberman@gmail.com>
2016-11-19 22:28:26 -0500

    move ToolTip to a separate module

--- gui.py
+++ gui.py
@@ -4,49 +4,7 @@ from tkinter import scrolledtext
 from tkinter import Menu
 from tkinter import messagebox as mbox
 
-
-class ToolTip:
-    def __init__(self, widget):
-        self.widget = widget
-        self.tipwindow = None
-        self.id = None
-        self.x = self.y = 0
-
-
-    def showtip(self, text):
-        """
-        Display text in a tooltip window.
-        """
-        self.text = text
-        if self.tipwindow or not self.text:
-            return
-        x, y, _cx, cy = self.widget.bbox('insert')
-        x = x + self.widget.winfo_rootx() + 27
-        y = y + cy + self.widget.winfo_rooty() + 27
-        self.tipwindow = tw = tk.Toplevel(self.widget)
-        tw.wm_overrideredirect(1)
-        tw.wm_geometry('+%d+%d' % (x, y))
-        label = tk.Label(tw, text=self.text, justify=tk.LEFT,
-            background='#ffffe0', relief=tk.SOLID, borderwidth=1,
-            font=('tahoma', '8', 'normal'))
-        label.pack(ipadx=1)
-
-    
-    def hidetip(self):
-        tw = self.tipwindow
-        self.tipwindow = None
-        if tw:
-            tw.destroy()
-
-
-def createToolTip(widget, text):
-    tooltip = ToolTip(widget)
-    def enter(event):
-        tooltip.showtip(text)
-    def leave(event):
-        tooltip.hidetip()
-    widget.bind('<Enter>', enter)
-    widget.bind('<Leave>', leave)
+import tooltip as tt
 
 
 class OOP:
@@ -118,7 +76,7 @@ class OOP:
             command=self._spin)
         self.spin.grid(column=0, row=2)
         # Add a tooltip
-        createToolTip(self.spin, 'This is a spin control.')
+        tt.createToolTip(self.spin, 'This is a spin control.')
 
         # Using a scrolled text control
         scrolW = 30
@@ -127,7 +85,7 @@ class OOP:
             self.monty, width=scrolW, height=scrolH, wrap=tk.WORD)
         self.scr.grid(column=0, row=5, columnspan=3, sticky='WE')
         # Add a tooltip
-        createToolTip(self.scr, 'This is a scrolled text widget.')
+        tt.createToolTip(self.scr, 'This is a scrolled text widget.')
 
 
     def tab2widgets(self):
